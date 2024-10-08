00919a97028818a3fa331f3b69c5fe3160ad43a5
sofieschn <sofie.schnitzer@gmail.com>
2024-10-08 15:02:05 +0200

    starting over from node 1

--- key.erl
+++ key.erl
@@ -1,22 +1,14 @@
 -module(key).
 -export([generate/0, between/3]).
 
-generate() ->
-    random:uniform(1000000000).
+-define(MAX, 1000).
 
-%% Check if a Key is between From and To or equal to To, this is called a partly closed interval and is denoted (From, To].
-between(Key, From, To) ->
-	if
-		%% From is less than to, and the key is between from and To, so we return true
-		(From < To) and (Key > From) and (Key =< To) ->
-		    true;
-		%% This is the case when from is actually bigger than To, which can happen as we have a ring structure
-		%% In this case, the key can either be bigger than From or smaller than To to be between these two values.
-		(From > To) and ((Key > From) or (Key =< To)) ->
-        	true;
-		%% From and to is the same node, return true
-		From == To ->
-	        true;
-	    true ->
-	        false
-    end.
+generate() ->
+    random:uniform(?MAX).
+
+between(Key, From, To) when From < To ->
+    From < Key andalso Key =< To;
+between(Key, From, To) when From > To ->
+    Key =< To orelse From < Key;
+between(Key, Id, Id) ->
+    true.
